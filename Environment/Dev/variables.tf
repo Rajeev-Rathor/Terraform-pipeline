21668cd1190a2501b5297e7052d83ff4c9e8def5
Rajeev-Rathor <singhrathor202@gmail.com>
2026-08-24 22:49:22 +0530

    block code

--- Environment/Dev/variables.tf
+++ Environment/Dev/variables.tf
@@ -1,20 +1,20 @@
 variable "r_group" {
-  description = "Resource Group Details"
+  # description = "Resource Group Details"
 
-  type = map(object({
-    name     = string
-    location = string
-  }))
+  # type = map(object({
+  #   name     = string
+  #   location = string
+  # }))
 }
 
 
 variable "v_ntrk" {
-  description = "Virtual Network Details"
+  # description = "Virtual Network Details"
 
-  type = map(object({
-    name                = string
-    location            = string
-    resource_group_name = string
-    address_space       = list(string)
-  }))
+  # type = map(object({
+  #   name                = string
+  #   location            = string
+  #   resource_group_name = string
+  #   address_space       = list(string)
+  # }))
 }
